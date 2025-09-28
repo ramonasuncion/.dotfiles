2f71ebb9a23da89eec8b2b8828e578ff7c94e5ef
Ramon Asuncion <asuncionbatista@gmail.com>
2025-09-28 16:22:22 -0400

    Do not override motions

--- nvim/lua/plugins/fzf.lua
+++ nvim/lua/plugins/fzf.lua
@@ -21,5 +21,6 @@ fzf.setup({
 })
 
 -- plugin-specific keymaps
-vim.keymap.set("n", "<C-d>", fzf.files)
-vim.keymap.set("n", "<C-S-d>", fzf.git_files)
+vim.keymap.set("n", "<leader>f", fzf.files)
+vim.keymap.set("n", "<leader>g", fzf.git_files)
+vim.keymap.set("n", "<leader>m", fzf.marks)
