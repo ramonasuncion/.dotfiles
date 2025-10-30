22904c06693687c855ba7b8448423e017361d32e
Ramon Asuncion <asuncionbatista@gmail.com>
2025-10-30 00:44:07 -0400

    Add autocomplete and diagnostics

--- nvim/lua/plugins/lspconfig.lua
+++ nvim/lua/plugins/lspconfig.lua
@@ -1,9 +1,37 @@
 -- lua/plugins/lspconfig.lua
+vim.keymap.set("i", "<Tab>", function()
+  if vim.fn.pumvisible() == 1 then
+    return vim.api.nvim_replace_termcodes("<C-y>", true, true, true)
+  else
+    return vim.api.nvim_replace_termcodes("<Tab>", true, true, true)
+  end
+end, { expr = true })
+
 local on_attach = function(client, bufnr)
   local opts = { buffer = bufnr }
-  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, { buffer = bufnr })
+
+  vim.keymap.set('n', 'gd', vim.lsp.buf.definition, opts)
   vim.keymap.set('n', 'K', vim.lsp.buf.hover, opts)
   vim.keymap.set('n', '<leader>rn', vim.lsp.buf.rename, opts)
+  vim.keymap.set('i', '<C-s>', vim.lsp.buf.signature_help, opts)
+
+  vim.lsp.completion.enable(true, client.id, bufnr, {
+    convert = function(item)
+      local abbr = item.label:gsub("%b()", ""):gsub("%b{}", "")
+      abbr = abbr:match("[%w_.]+.*") or abbr
+      abbr = #abbr > 15 and abbr:sub(1, 14) .. "…" or abbr
+
+      local menu = item.detail or ""
+      menu = #menu > 15 and menu:sub(1, 14) .. "…" or menu
+
+      return { abbr = abbr, menu = menu }
+    end,
+  })
+
+  vim.api.nvim_create_autocmd("CursorHoldI", {
+    buffer = bufnr,
+    callback = function() vim.lsp.buf.signature_help() end,
+  })
 end
 
 vim.lsp.config("clangd", {
@@ -13,3 +41,4 @@ vim.lsp.config("clangd", {
 })
 
 vim.lsp.enable("clangd")
+
