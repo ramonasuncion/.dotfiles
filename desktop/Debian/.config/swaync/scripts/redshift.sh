1114bda9266e81d485fc4c143823dfa29055e5c3
Ramon Asuncion <asuncionbatista@gmail.com>
2025-11-12 05:35:55 -0500

    Add redshift.sh

--- desktop/Debian/.config/swaync/scripts/redshift.sh
+++ desktop/Debian/.config/swaync/scripts/redshift.sh
@@ -2,4 +2,4 @@
 CONTENT=$(curl -s http://ip-api.com/json/)
 longitude=$(echo $CONTENT | jq .lon)
 latitude=$(echo $CONTENT | jq .lat)
-wlsunset -l $latitude -L $longitude 
+wlsunset -l $latitude -L $longitude
